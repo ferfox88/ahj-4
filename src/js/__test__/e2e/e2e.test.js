f68f458726964105b911e93056bfa2907bc0dfdb
ferfox88 <ferfox@yandex.ru>
2022-06-30 22:03:59 +0300

    4 commit

---
+++ src/js/__test__/e2e/e2e.test.js
@@ -0,0 +1,25 @@
+import puppetteer from 'puppeteer';
+
+jest.setTimeout(30000); // default puppeteer timeout
+
+describe('Credit Card Validator form', () => {
+  let browser = null;
+  let page = null;
+  const baseUrl = 'http://localhost:8888';
+  beforeAll(async () => {
+    browser = await puppetteer.launch({
+      headless: false,
+      slowMo: 100,
+      devtools: true,
+    });
+    page = await browser.newPage();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+
+  test('should add do something', async () => {
+    await page.goto(baseUrl);
+  });
+});
